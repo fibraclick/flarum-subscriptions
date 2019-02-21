156a815b6c9e8d430655f5f99f6946b1e7c122cc
Matteo Contrini <matteo@contrini.it>
2019-02-21 23:54:44 +0100

    html email and translation to Italian

--- views/emails/newPost.blade.php
+++ views/emails/newPost.blade.php
@@ -1,8 +1,7 @@
-Hey {!! $user->display_name !!}!
+Ciao {!! $user->display_name !!}!
 
-{!! $blueprint->post->user->display_name !!} made a post in a discussion you're following: {!! $blueprint->post->discussion->title !!}
+{!! $blueprint->post->user->display_name !!} ha risposto alla discussione "{!! $blueprint->post->discussion->title !!}"
 
-To view the new activity, check out the following link:
 {!! app()->url() !!}/d/{!! $blueprint->post->discussion_id !!}/{!! $blueprint->post->number !!}
 
 ---
@@ -11,4 +10,4 @@ To view the new activity, check out the following link:
 
 ---
 
-You won't receive any more notifications about this discussion until you're up-to-date.
+Non riceverai ulteriori notifiche per questa discussione finché non avrai visualizzato i nuovi messaggi.
