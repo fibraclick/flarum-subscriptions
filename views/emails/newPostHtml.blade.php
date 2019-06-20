83964f639e54276167b58153e6d4e97da5b0a4f4
Matteo Contrini <matteo@contrini.it>
2019-06-20 22:30:52 +0200

    Update template

--- views/emails/newPostHtml.blade.php
+++ views/emails/newPostHtml.blade.php
@@ -2,13 +2,12 @@ Ciao {!! $user->display_name !!}!
 <br><br>
 <i>{!! $blueprint->post->user->display_name !!}</i> ha risposto alla discussione
 <a href="{!! app()->url() !!}/d/{!! $blueprint->post->discussion_id !!}/{!! $blueprint->post->number !!}">
-<i>{!! $blueprint->post->discussion->title !!}</i>.
-</a>
+<i>{!! $blueprint->post->discussion->title !!}</i></a>.
 <br><br>
 ---
-<br><br>
+<br>
 {!! $blueprint->post->contentHtml !!}
-<br><br>
+<br>
 ---
 <br><br>
 <i>Non riceverai ulteriori notifiche per questa discussione finché non avrai visualizzato i nuovi messaggi.</i>
